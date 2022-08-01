edbcab9fe5fbc2ec47966154787e9c0fa5d6da06
eyal <narnavg@gmail.com>
2022-08-01 12:32:09 +0300

    first commit

--- test.py
+++ test.py
@@ -7,8 +7,15 @@ from kivy.app import App
 from kivy.uix.button import Button
 from functools import partial
 from kivy.clock import Clock
+import sqlite3
 
 def my_callback(value, key, *largs):
+    cur = con.cursor()
+
+    # Create table
+    cur.execute('''CREATE TABLE if not exists crypto (date text,  price real)''')
+
+    
     # def animate(self, instance):
     # read data from API (crypto)
     x = requests.get('https://www.deribit.com/api/v2/public/get_index_price?index_name=eth_usd')
@@ -20,18 +27,27 @@ def my_callback(value, key, *largs):
     print(y["result"]["index_price"])
     # saev data to local variable
     price= y["result"]["index_price"]
+    # Insert a row of data
+    cur.execute(f"INSERT INTO crypto VALUES ('2006-01-05',{price})")
+
+    # Save (commit) the changes
+    con.commit()
+
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    # con.close()
 
     # Opening JSON file (read history)
-    f = open('sample.json')
+    # f = open('sample.json')
 
-    # save all data to file
-    data = json.load(f)
-    data.append({"price": price})
-    json_object = json.dumps(data, indent=4)
-    with open("sample.json", "w") as outfile:
-        outfile.write(json_object)
+    # # save all data to file
+    # data = json.load(f)
+    # data.append({"price": price})
+    # json_object = json.dumps(data, indent=4)
+    # with open("sample.json", "w") as outfile:
+    #     outfile.write(json_object)
 
-
+con = sqlite3.connect('example.db')
 Clock.schedule_interval(partial(my_callback, 'my value', 'my key'), 2)
 
 class TestApp(App):
